3a6b2293d27d128ac2b6ea38030534d11d28f548
matteushc <matteus_h@hotmail.com>
2025-10-29 13:13:18 -0300

    Refactor EMR DAG with custom job flow operator
    Refactor EMR DAG to use a custom operator and improve task structure.

--- dag_create_cluster.py
+++ dag_create_cluster.py
@@ -1,66 +1,130 @@
+from __future__ import annotations
+from datetime import datetime
+import os
 import json
+import textwrap
 import logging
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.utils.dates import days_ago
+import pytz
 from airflow.decorators import dag, task, task_group
-from airflow.utils.task_group import TaskGroup
+from airflow.exceptions import AirflowException
+from airflow.hooks.package_index import PackageIndexHook
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.models import Variable
+from hermond.notifications import AWSNotifier
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
 from airflow.providers.amazon.aws.operators.emr import (
     EmrCreateJobFlowOperator,
     EmrAddStepsOperator,
 	EmrTerminateJobFlowOperator
 )
 from airflow.utils.trigger_rule import TriggerRule
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
-from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.emr import EmrHook
+from airflow.utils.context import Context
+from botocore.config import Config
 
-# DAG defaults
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    'provide_context': True
-}
 
-with DAG(
-    dag_id='example_emr_dag',
-    default_args=default_args,
-    description='An example DAG for AWS EMR',
-    schedule_interval=None,
-    start_date=datetime(2025, 9, 1),
-    catchup=False,
-    tags=['example', 'emr'],
-) as dag:
+ENV = Variable.get("ENVIRONMENT").lower()
+DAG_TAGS = ["emr"]
 
-    # Task to read JSON from S3
-    def read_json_from_s3(bucket_name, key):
-        """
-        Reads a JSON file from S3 and returns its content as a dictionary.
-        """
-        s3_hook = S3Hook(aws_conn_id='aws_default')
-        file_content = s3_hook.read_key(key=key, bucket_name=bucket_name)
-        return json.loads(file_content)
+
+def read_json_from_s3(bucket_name, key):
+    """
+    Reads a JSON file from S3 and returns its content as a dictionary.
+    """
+    s3_hook = S3Hook(aws_conn_id='aws_default')
+    file_content = s3_hook.read_key(key=key, bucket_name=bucket_name)
+    return json.loads(file_content)
+
+
+class CustomEmrCreateJobFlowOperator(EmrCreateJobFlowOperator):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def execute(self, context):
+        
+        boto3_config = Config(
+            retries = {
+                'max_attempts': 12,
+                'mode': 'standard'
+            }
+        )
+        emr = EmrHook(
+            aws_conn_id=self.aws_conn_id,
+            emr_conn_id=self.emr_conn_id,
+            region_name=self.region_name,
+            config=boto3_config 
+        )
+
+        self.log.info(
+            'Creating JobFlow using aws-conn-id: %s, emr-conn-id: %s',
+            self.aws_conn_id, self.emr_conn_id
+        )
+        response = emr.create_job_flow(self.job_flow_overrides)
+
+        if not response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            raise AirflowException('JobFlow creation failed: %s' % response)
+        else:
+            self.log.info('JobFlow with id %s created', response['JobFlowId'])
+            return response['JobFlowId']
+
+
+def on_failure_callback(context):
+    ti = context["task_instance"]
+
+    msg = f"Falha na DAG {ti.dag_id}, Task {ti.task_id}"
+    logging.error(f"Failed to send Teams message: {msg}")
+
+
+@dag(
+    dag_id = "test_emr",
+    dag_display_name = "Test EMR",
+    description = __doc__.partition(".")[0],
+    start_date = datetime(2024, 11, 19),
+    schedule = "0 9 * * 3" 
+    catchup = False,
+    max_active_runs = 1,
+    # DAG default arguments
+    default_args = {
+        "owner": "root",
+        "depends_on_past": False,
+        "email_on_failure": False,
+        "on_failure_callback": on_failure_callback
+    },
+    tags = DAG_TAGS
+)
+def dag():
 
     # Define the S3 bucket and key
-    S3_BUCKET_NAME = 'bucket_name'
-    S3_KEY = 'scripts_jobs/template.json'
-    job_flow_overrides = read_json_from_s3(S3_BUCKET_NAME, S3_KEY)
+    S3_BUCKET_NAME = 'basic-bucket'
+    S3_KEY = 'scripts_jobs/cluster_large_r7_dag.json'
 
+    lista_template_clusters = {
+        "cluster_small_ccf": "scripts_jobs/config_temp/cluster_small_ccf.json",
+        "cluster_small_lawsuit": "scripts_jobs/config_temp/cluster_small_lawsuit.json",
+        "cluster_small_consultas": "scripts_jobs/config_temp/cluster_small_consultas.json"
+    }
+
+    #job_flow_overrides = read_json_from_s3(S3_BUCKET_NAME, S3_KEY)
 
     @task
-    def generate_list():
+    def get_list_of_aggregations():
+        
         list_cluster = [
-            {"cluster_name": "cluster_1", "file_name": "s3://bucket_name/script_1.py"},
-            {"cluster_name": "cluster_2", "file_name": "s3://bucket_name/script_2.py"}
+            {"file_name": "s3://basic-bucket/scripts_jobs/test_run.py", "args1": "--demand_id", "args2": "xx1", "cluster_type": "cluster_small_xx1"},
+            {"file_name": "s3://basic-bucket/scripts_jobs/test_run.py", "args1": "--demand_id", "args2": "xx2", "cluster_type": "cluster_small_xx2"},
+            {"file_name": "s3://basic-bucket/scripts_jobs/test_run.py", "args1": "--demand_id", "args2": "xx3", "cluster_type": "cluster_small_xx3"}
         ]
         return list_cluster
 
+    # Execute Glue Job for metadata generation (Job0)
+    list_of_aggregations = get_list_of_aggregations()
+
     @task
     def emit_step(config):
+
         spark_steps = [{
                 'Name': 'Run Spark Job',
                 'ActionOnFailure': 'CONTINUE',
@@ -69,22 +133,32 @@ with DAG(
                     'Args': [
                         'spark-submit',
                         '--deploy-mode', 'cluster',
-                        config['file_name']
+                        config['file_name'],
+                        config['args1'], config['args2']
                     ]
                 }
             }]
         return spark_steps
 
 
-    @task_group(group_id="group_create_emr")
-    def create_emr_task_group(config, **kwargs):
+    @task
+    def create_template(config):
 
-        print(kwargs)
-        create = EmrCreateJobFlowOperator(
+        run_cluster_flow = config['cluster_type']
+        cluster_file = lista_template_clusters[run_cluster_flow]
+
+        job_flow_overrides = read_json_from_s3('basic-bucket', cluster_file)
+        return job_flow_overrides
+
+
+    @task_group(group_id="group_create_emr")
+    def create_emr_task_group(config):
+
+        create = CustomEmrCreateJobFlowOperator(
             task_id=f'create_cluster',
-            job_flow_overrides=job_flow_overrides,
+            job_flow_overrides=create_template(config),
             aws_conn_id='aws_default',
-            retries=1
+            retries=0
         )
 
         add_steps = EmrAddStepsOperator(
@@ -92,7 +166,7 @@ with DAG(
             job_flow_id=create.output,
             steps=emit_step(config),
             aws_conn_id='aws_default',
-            retries=0
+            retries=3
         )
 
         watch_step = EmrStepSensor(
@@ -100,25 +174,21 @@ with DAG(
             job_flow_id=create.output,
             step_id="{{ task_instance.xcom_pull(task_ids='group_create_emr.add_steps', map_indexes=task_instance.map_index, key='return_value')[0] }}",
             aws_conn_id='aws_default',
-            retries=0
+            poke_interval=180,
+            timeout=300,
+            retries=3,
+            deferrable=True
         )
 
         create >> add_steps >> watch_step
-
-    lista = generate_list()
     
-    create_emr = create_emr_task_group.expand(config=lista)
+    create_emr = create_emr_task_group.expand(config=list_of_aggregations)
 
+    create_emr
 
-    # Final task after all sensors
-    def finalize_process(**kwargs):
-        logging.info("All EMR jobs completed. Finalizing process...")
-
-    finalize = PythonOperator(
-        task_id='finalize_process',
-        python_callable=finalize_process,
-        provide_context=True,
-        dag=dag,
-    )
-
-    create_emr >> finalize
+# Entry Point to DAG
+if __name__ == '__main__':
+    dag().test(mark_success_pattern=".*")
+    # dag()
+else:
+    dag()
